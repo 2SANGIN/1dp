9aef9d844464799db26b9799f98825ca7f4e6fcd
atmost1815 <silee9019@gmail.com>
2018-04-06 00:59:55 +0900

    11057번: 오르막 수 완성 (리액티브)

--- src/io/github/atmost1815/acmicpc/ascendingnumber/Main.java
+++ src/io/github/atmost1815/acmicpc/ascendingnumber/Main.java
@@ -12,32 +12,12 @@ public class Main {
     private static Scanner sc = new Scanner(System.in);
 
     public static void main(final String[] args) {
-        final int tc = sc.nextInt();
-
-        IntStream.of(IntStream.rangeClosed(tc + 1, tc + 9)
-                .reduce(1, (left, right) -> left * right % 10_007))
+        IntStream.of(
+                IntStream.of(sc.nextInt())
+                        .flatMap(tc -> IntStream.rangeClosed(tc + 1, tc + 9))
+                        .reduce(1, (left, right) -> left * right % 10_007))
                 .map(v -> v * 6995 % 10_007)
                 .forEach(System.out::print);
-
-        final int[][] dp = new int[tc][10];
-        int result = 55;
-        for (int i = 1; i < tc; i++) {
-            dp[i][0] = result;
-            if (i + 1 == tc) {
-                break;
-            }
-
-            for (int j = 1; j < 8; j++) {
-                dp[i][j] = dp[i][j - 1] - dp[i - 1][j - 1];
-                if (dp[i][j] < 0) {
-                    dp[i][j] += 10_007;
-                }
-                result += dp[i][j];
-            }
-            result += i + 3;
-            result %= 10_007;
-        }
-        System.out.println(dp[tc - 1][0]);
     }
 
     static class Scanner {
